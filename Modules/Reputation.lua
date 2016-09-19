fe82b22a571a4f8cacf727b058531cf5af85cc79
Predicate <predicate.dev@gmail.com>
2016-09-18 20:29:10 -0700

    Fix Reputation.lua properly

--- Modules/Reputation.lua
+++ Modules/Reputation.lua
@@ -4,15 +4,11 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("UPDATE_FACTION")
 local reps = {}
 frame:SetScript("OnEvent", function()
-    local name, description, standingID, barMin, barMax, barValue, atWarWith, canToggleAtWar, isHeader,
-    isCollapsed, hasRep, isWatched, isChild, factionID, hasBonusRepGain, canBeLFGBonus, _
-    local difference
     for factionIndex=1, GetNumFactions() do
-        name, description, standingID, barMin, barMax, barValue, atWarWith, canToggleAtWar, isHeader,
-    isCollapsed, hasRep, isWatched, isChild, factionID, hasBonusRepGain, canBeLFGBonus = GetFactionInfo(factionIndex)
-        if not isHeader and name then
+    local name, _, standingID, bottomValue, topValue, earnedValue, _, _, isHeader = GetFactionInfo(factionIndex)
+        if name and not isHeader then
             if (reps[name]) then
-                difference = earnedValue - reps[name]
+               local difference = earnedValue - reps[name]
                 if (difference > 0) then
                     --ChatFrame5:AddMessage(format("Reputation with %s increased by %d. (%d remaining until %s)", --long version
                     ChatFrame5:AddMessage(format("%s + %d. (%d until %s)", name, difference, topValue - earnedValue, (standingID == 8) and "max" or _G["FACTION_STANDING_LABEL"..standingID + 1]), 0.5, 0.5, 1)
